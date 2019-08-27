345de9350fcc56595203af6a6a22fd58567db322
Marcus Lira <marcuslira@gmail.com>
2019-08-27 02:09:10 +0200

    move go envs to path

--- .zshrc
+++ .zshrc
@@ -66,11 +66,11 @@ export LC_ALL=en_US.UTF-8
 export LANG=en_US.UTF-8
 
 # Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='mvim'
-# fi
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
 
 # Compilation flags
 # export ARCHFLAGS="-arch x86_64"
@@ -92,11 +92,6 @@ export LANG=en_US.UTF-8
 
 # for direnv
 eval "$(direnv hook zsh)"
-
-# for go
-export GOPATH=$HOME/go
-export GOBIN=$GOPATH/bin
-export PATH=$PATH:$GOBIN
 
 # -- Term ----------------------------------------------------------------------
 TERM=xterm-256color
--- path.zsh
+++ path.zsh
@@ -4,9 +4,6 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
-# Make sure PHP 7.1 is loaded
-#export PATH="$(brew --prefix homebrew/php/php71)/bin:$PATH"
-
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
@@ -26,3 +23,8 @@ export NVM_DIR=~/.nvm
 export PATH=/usr/local/anaconda3/bin:"$PATH"
 export PATH=/usr/local/share/dotnet:"$PATH"
 #export PATH=/usr/local/mysql-shell/bin:"$PATH"
+
+# for go
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
